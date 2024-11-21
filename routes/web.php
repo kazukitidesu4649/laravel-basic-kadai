6ff0b6aff91305813c96c831c5b2a93273822734
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-11-22 00:57:40 +0900

    web.phpを復元

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// ルーティングを設定するコントローラの宣言 (復元)
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\StoreController;
@@ -20,6 +22,18 @@ use App\Http\Controllers\StoreController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/hello', [HelloController::class, 'index']);
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
